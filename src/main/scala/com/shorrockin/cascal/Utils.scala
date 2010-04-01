e7d5a62e322753a9bc06f58a08d84339ed56618c
cshorrock <cshorrock@D1010862.eac.ad.ea.com>
2010-04-01 11:47:14 -0700

    cleaned up the embedded instance for unit-testing

---
+++ src/main/scala/com/shorrockin/cascal/Utils.scala
@@ -0,0 +1,86 @@
+package com.shorrockin.cascal
+
+import org.apache.cassandra.io.util.FileUtils
+import _root_.scala.io.Source
+import java.io.{FileWriter, InputStream, FileOutputStream, File}
+
+/**
+ * common utility functions that don't fit elsewhere.
+ */
+object Utils extends Logging {
+
+  /**
+   * recursively deletes a directory and all of it's contents
+   */
+  def delete(directory:File):Boolean = {
+    if (directory.exists && directory.isDirectory) {
+      val result = directory.listFiles.foldLeft(true) { (left, right) => delete(right) && left }
+      val out = directory.delete
+      log.debug("deletion attempt on directory: " + directory.getCanonicalPath + " - " + out)
+      out
+    } else {
+      val out = directory.delete
+      log.debug("deletion attempt on file: " + directory.getCanonicalPath + " - " + out)
+      out
+    }
+  }
+
+
+  /**
+   * copies the specified resource in the classpath to the specified.
+   * resource should generally start with a "/".
+   */
+  def copy(is:InputStream, file:File):File = {
+    var out = new FileOutputStream(file)
+    var buf = new Array[Byte](1024)
+    var len = 0
+
+    manage(out, is) {
+      while (-1 != len) {
+        len = is.read(buf, 0, buf.length)
+        if (-1 != len) out.write(buf, 0, len)
+      }
+      out.flush
+    }
+
+    file
+  }
+
+
+  /**
+   * replaces all instances of the specified token with the specified replacement
+   * file in the source file.
+   */
+  def replace(file:File, replacements:(String, String)*):File = {
+    val contents = Source.fromFile(file).getLines.toList.map { (line) =>
+      var current = line
+      replacements.foreach { (r) => current = current.replace(r._1, r._2) }
+      current
+    }
+
+    val writer = new FileWriter(file)
+    manage(writer) {
+      contents.foreach { writer.write(_) }
+      writer.flush
+    }
+
+    file
+  }
+
+
+  /**
+   *  simple function to ignore any error which occurs
+   */
+  def ignore(f: => Unit):Unit = try { f } catch { case e:Throwable => /* ignore */ }
+
+
+  /**
+   *  performs the close method on the specified object(s) after
+   * the specified function has been called
+   */
+  def manage(closeable:{ def close() }*)(f: => Unit) {
+    try { f } finally {
+      closeable.foreach { (c) => ignore(c.close()) }
+    }
+  }
+}
--- src/test/scala/com/shorrockin/cascal/EmbeddedCassandra.scala
+++ src/test/scala/com/shorrockin/cascal/EmbeddedCassandra.scala
@@ -1,25 +1,86 @@
 package com.shorrockin.cascal
 
-import me.prettyprint.cassandra.testutils.EmbeddedServerHelper
+import org.apache.cassandra.thrift.CassandraDaemon
+import org.apache.cassandra.config.DatabaseDescriptor
+import java.io.File
+
+
 
 /**
  * trait which mixes in the functionality necessary to embed
  * cassandra into a unit test
  */
-trait EmbeddedCassandra {
-  def boot(f:(Session) => Unit) = {
-    val embedded                = new EmbeddedServerHelper
-    var session:Option[Session] = None
+trait EmbeddedCassandra extends Logging {
+  import Utils._
 
-    try {
-      embedded.setup
-      session = Some(new Session("localhost", 9160, Consistency.One))
-      f(session.get)
-    } finally {
-      if (session.isDefined) session.get.close
-      embedded.shutdown
+  def boot(f:(Session) => Unit) = {
+    EmbeddedCassandra.init
+    val session = new Session("localhost", 9160, Consistency.One)
+    manage(session) { f(session) }
+  }
+}
+
+/**
+ * maintains the single instance of the cassandra server
+ */
+object EmbeddedCassandra extends Logging {
+  import Utils._
+  var initialized = false
+
+  def init = synchronized {
+    if (!initialized) {
+      val homeDirectory = new File("cassandra.home.unit-tests")
+      delete(homeDirectory)
+      homeDirectory.mkdirs
+
+      log.debug("creating cassandra instance at: " + homeDirectory.getCanonicalPath)
+      log.debug("copying cassandra configuration files to root directory")
+    
+      val fileSep     = System.getProperty("file.separator")
+      val storageFile = new File(homeDirectory, "storage-conf.xml")
+      val logFile     = new File(homeDirectory, "log4j.properties")
+
+      replace(copy(resource("/storage-conf.xml"), storageFile), ("%temp-dir%" -> (homeDirectory.getCanonicalPath + fileSep)))
+      copy(resource("/log4j.properties"), logFile)
+
+      System.setProperty("storage-config", homeDirectory.getCanonicalPath)
+
+      log.debug("creating data file and log location directories")
+      DatabaseDescriptor.getAllDataFileLocations.foreach { (file) => new File(file).mkdirs }
+      new File(DatabaseDescriptor.getLogFileLocation).mkdirs
+
+      val daemon = new CassandraDaemonThread
+      daemon.start
+
+      initialized = true
     }
   }
 
-
+  private def resource(str:String) = classOf[EmbeddedCassandra].getResourceAsStream(str)
 }
+
+/**
+ * daemon thread used to start and stop cassandra
+ */
+class CassandraDaemonThread extends Thread("CassandraDaemonThread") with Logging {
+  private val daemon = new CassandraDaemon
+
+  setDaemon(true)
+
+  override def run:Unit = {
+    log.debug("initializing cassandra daemon")
+    daemon.init(new Array[String](0))
+    log.debug("starting cassandra daemon")
+    daemon.start
+    Thread.sleep(1000)
+  }
+
+  def close():Unit = {
+    log.debug("instructing cassandra deamon to shut down")
+    daemon.stop
+    log.debug("blocking on cassandra shutdown")
+    this.join
+    log.debug("cassandra shut down")
+  }
+}
+
